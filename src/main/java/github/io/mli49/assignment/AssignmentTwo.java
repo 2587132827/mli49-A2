371a982ad8324eab8c19c2b159d24bac30c2c4b4
2587132827 <2587132827@qq.com>
2024-12-16 05:08:06 +0800

    part5 6 finished

--- src/main/java/github/io/mli49/assignment/AssignmentTwo.java
+++ src/main/java/github/io/mli49/assignment/AssignmentTwo.java
@@ -4,11 +4,14 @@ package github.io.mli49.assignment;
 public class AssignmentTwo {
     // main method in AssignmentTwo class
     public static void main(String[] args) {
-//        partThree();
-//        partFourA();
-         partFourB();
+        AssignmentTwo assignmentTwo = new AssignmentTwo();
+//        assignmentTwo.partThree();
+//        assignmentTwo.partFourA();
+//        assignmentTwo.partFourB();
+//        assignmentTwo.partFive();
+        assignmentTwo.partSix();
     }
-    public static void partThree(){
+    public  void partThree(){
         // Create a new Employee who will operate the ride
         github.io.mli49.assignment.part3.Employee rideOperator = new github.io.mli49.assignment.part3.
                     Employee("John", 30, "Male", "Ride Operator", "Roller Coaster");
@@ -40,7 +43,7 @@ public class AssignmentTwo {
         // Print the updated queue after removal
         rollerCoaster.printQueue();
     }
-    public static void partFourA() {
+    public  void partFourA() {
         // Create a new Employee who will operate the ride
         github.io.mli49.assignment.part4a.Employee rideOperator = new github.io.mli49.assignment.part4a.Employee("John", 30, "Male", "Ride Operator", "Roller Coaster");
 
@@ -71,7 +74,7 @@ public class AssignmentTwo {
         // Print all visitors in the ride history
         rollerCoaster.printRideHistory();
     }
-    public static void partFourB(){
+    public  void partFourB(){
         // Create a new Employee who will operate the ride
         github.io.mli49.assignment.part4b.Employee rideOperator = new github.io.mli49.assignment.part4b.Employee("John", 30, "Male", "Ride Operator", "Roller Coaster");
 
@@ -107,8 +110,78 @@ public class AssignmentTwo {
         rollerCoaster.printRideHistory();
     }
     public void partFive(){
+        // Create a new Employee who will operate the ride
+        github.io.mli49.assignment.part5.Employee rideOperator = new github.io.mli49.assignment.part5.Employee("John", 30, "Male", "Ride Operator", "Roller Coaster");
+
+        // Create a new Ride object with a maximum of 3 riders per cycle
+        github.io.mli49.assignment.part5.Ride rollerCoaster = new github.io.mli49.assignment.part5.Ride("Roller Coaster", "Thrill", rideOperator, 3);
+
+        // Create and add visitors to the queue
+        github.io.mli49.assignment.part5.Visitor visitor1 = new github.io.mli49.assignment.part5.Visitor("KiKi", 25, "Female", "VIP",0);
+        github.io.mli49.assignment.part5.Visitor visitor2 = new github.io.mli49.assignment.part5.Visitor("Sam", 22, "Male", "Regular",0);
+        github.io.mli49.assignment.part5.Visitor visitor3 = new github.io.mli49.assignment.part5.Visitor("Tommy", 28, "Male", "Regular",0);
+        github.io.mli49.assignment.part5.Visitor visitor4 = new github.io.mli49.assignment.part5.Visitor("Jully", 35, "Male", "VIP",0);
+        github.io.mli49.assignment.part5.Visitor visitor5 = new github.io.mli49.assignment.part5.Visitor("kenny", 29, "Female", "Regular",0);
+        github.io.mli49.assignment.part5.Visitor visitor6 = new github.io.mli49.assignment.part5.Visitor("Frank", 31, "Male", "Regular",0);
+        github.io.mli49.assignment.part5.Visitor visitor7 = new github.io.mli49.assignment.part5.Visitor("Zark", 27, "Female", "VIP",0);
+        github.io.mli49.assignment.part5.Visitor visitor8 = new github.io.mli49.assignment.part5.Visitor("Hannah", 30, "Female", "Regular",0);
+        github.io.mli49.assignment.part5.Visitor visitor9 = new github.io.mli49.assignment.part5.Visitor("Coco", 26, "Female", "VIP",0);
+        github.io.mli49.assignment.part5.Visitor visitor10 = new github.io.mli49.assignment.part5.Visitor("Yoxi", 33, "Male", "Regular",0);
+
+        // Add visitors to the queue
+        rollerCoaster.addVisitorToQueue(visitor1);
+        rollerCoaster.addVisitorToQueue(visitor2);
+        rollerCoaster.addVisitorToQueue(visitor3);
+        rollerCoaster.addVisitorToQueue(visitor4);
+        rollerCoaster.addVisitorToQueue(visitor5);
+        rollerCoaster.addVisitorToQueue(visitor6);
+        rollerCoaster.addVisitorToQueue(visitor7);
+        rollerCoaster.addVisitorToQueue(visitor8);
+        rollerCoaster.addVisitorToQueue(visitor9);
+        rollerCoaster.addVisitorToQueue(visitor10);
+
+        // Print all visitors in the queue before running the cycle
+        System.out.println("Visitors in the queue before running the cycle:");
+        rollerCoaster.printQueue();
+
+        // Run one cycle of the ride
+        rollerCoaster.runOneCycle();
+
+        // Print all visitors in the queue after one cycle is run
+        System.out.println("\nVisitors in the queue after running the cycle:");
+        rollerCoaster.printQueue();
+
+        // Print all visitors in the ride history
+        System.out.println("\nVisitors in the ride history:");
+        rollerCoaster.printRideHistory();
     }
     public void partSix(){
+        // Create a new Employee who will operate the ride
+        github.io.mli49.assignment.part6.Employee rideOperator = new github.io.mli49.assignment.part6.Employee("John", 30, "Male", "Ride Operator", "Roller Coaster");
+
+        // Create a new Ride object
+        github.io.mli49.assignment.part6.Ride rollerCoaster = new github.io.mli49.assignment.part6.Ride("Roller Coaster", "Thrill", rideOperator, 3);
+
+        // Create some Visitors
+        github.io.mli49.assignment.part6.Visitor visitor1 = new github.io.mli49.assignment.part6.Visitor("KiKi", 25, "Female", "VIP",0);
+        github.io.mli49.assignment.part6.Visitor visitor2 = new github.io.mli49.assignment.part6.Visitor("Bill", 22, "Male", "Regular",0);
+        github.io.mli49.assignment.part6.Visitor visitor3 = new github.io.mli49.assignment.part6.Visitor("Sam", 28, "Male", "Regular",0);
+        github.io.mli49.assignment.part6.Visitor visitor4 = new github.io.mli49.assignment.part6.Visitor("Zuul", 35, "Male", "VIP",0);
+        github.io.mli49.assignment.part6.Visitor visitor5 = new github.io.mli49.assignment.part6.Visitor("Jack", 29, "Female", "Regular",0);
+        github.io.mli49.assignment.part6.Visitor visitor6 = new github.io.mli49.assignment.part6.Visitor("Jack", 22, "Female", "Regular",0);
+
+
+        // Add visitors to the ride history
+        rollerCoaster.addVisitorToHistory(visitor1);
+        rollerCoaster.addVisitorToHistory(visitor2);
+        rollerCoaster.addVisitorToHistory(visitor3);
+        rollerCoaster.addVisitorToHistory(visitor4);
+        rollerCoaster.addVisitorToHistory(visitor5);
+        rollerCoaster.addVisitorToHistory(visitor6);
+
+        // Export the ride history to a file
+        String fileName = "history.txt";
+        rollerCoaster.exportRideHistory(fileName);
     }
     public void partSeven(){
     }
---
+++ src/main/java/github/io/mli49/assignment/part6/Ride.java
@@ -0,0 +1,257 @@
+package github.io.mli49.assignment.part6;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Ride implements RideInterface {
+    // 3 instance variables
+    /**
+     * The name of the ride
+     */
+    private String rideName;
+    /**
+     * The type of the ride
+     */
+    private String rideType;
+    /**
+     * The employee who is a ride operator in charge running the ride
+     */
+    private Employee rideOperator;
+    /**
+     * The queue of visitors waiting to ride
+     */
+    private final Queue<Visitor> visitorQueue;
+
+    /**
+     * The list of visitors who have ridden the ride
+     */
+    private LinkedList<Visitor> visitorsRidden;
+    // part5 instance variables
+    /**
+     * The maximum number of visitors per cycle
+     */
+    private int maxRider;
+    /**
+     * The number of times the ride has been run
+     */
+    private int numOfCycles;
+    // Default constructor
+    public Ride() {
+        visitorQueue = new LinkedList<>();
+        visitorsRidden = new LinkedList<>();
+    }
+
+    // Constructor with parameters
+    public Ride(String rideName, String rideType, Employee rideOperator, int maxRider) {
+        this.rideName = rideName;
+        this.rideType = rideType;
+        this.rideOperator = rideOperator;
+        this.maxRider = maxRider;
+        this.numOfCycles = 0;
+        visitorQueue = new LinkedList<>();
+        visitorsRidden = new LinkedList<>();
+    }
+
+    // Getters and setters
+
+    public int getMaxRider() {
+        return maxRider;
+    }
+
+    public void setMaxRider(int maxRider) {
+        this.maxRider = maxRider;
+    }
+
+    public int getNumOfCycles() {
+        return numOfCycles;
+    }
+
+    public void setNumOfCycles(int numOfCycles) {
+        this.numOfCycles = numOfCycles;
+    }
+    public String getRideName() {
+        return rideName;
+    }
+
+    public void setRideName(String rideName) {
+        this.rideName = rideName;
+    }
+
+    public String getRideType() {
+        return rideType;
+    }
+
+    public void setRideType(String rideType) {
+        this.rideType = rideType;
+    }
+
+    public Employee getRideOperator() {
+        return rideOperator;
+    }
+    /**
+     * Set the ride operator for the ride
+     * @param rideOperator the employee who is a ride operator
+     */
+    public void setRideOperator(Employee rideOperator) {
+        this.rideOperator = rideOperator;
+    }
+
+    // Additional method to display ride info
+    public String getRideInfo() {
+        return "Ride: " + rideName + ", Type: " + rideType + ", Operator: " + rideOperator.getName();
+    }
+    // part3 methods
+    /**
+     * Add a visitor to the queue for the ride
+     * @param visitor the visitor to add to the queue
+     */
+    public void addVisitorToQueue(Visitor visitor) {
+        visitorQueue.add(visitor);
+        System.out.println(visitor.getName() + " has joined the queue for " + rideName);
+    }
+    /**
+     * Remove a visitor from the queue for the ride
+     */
+    public void removeVisitorFromQueue() {
+        Visitor removedVisitor = visitorQueue.poll();
+        if (removedVisitor != null) {
+            System.out.println(removedVisitor.getName() + " has been removed from the queue for " + rideName);
+        } else {
+            System.out.println("The queue for " + rideName + " is empty.");
+        }
+    }
+    /**
+     * Print the queue for the ride
+     */
+    public void printQueue() {
+        if (visitorQueue.isEmpty()) {
+            System.out.println("The queue for " + rideName + " is empty.");
+        } else {
+            System.out.println("Queue for " + rideName + ":");
+            for (Visitor visitor : visitorQueue) {
+                System.out.println("- " + visitor.getName());
+            }
+        }
+    }
+
+    // part5 methods
+    /**
+     * Add a visitor to the queue
+     */
+    @Override
+    public void runOneCycle() {
+        // If no ride operator is assigned to the ride, the ride cannot be run, and a message is
+        //printed out
+        if (rideOperator == null) {
+            System.out.println("Cannot run the ride: No ride operator assigned.");
+            return;
+        }
+        // If the queue is empty, the ride cannot be run, and a message is printed out
+        if (visitorQueue.isEmpty()) {
+            System.out.println("Cannot run the ride: No visitors in the queue.");
+            return;
+        }
+
+        int ridersThisCycle = Math.min(visitorQueue.size(), maxRider);
+        for (int i = 0; i < ridersThisCycle; i++) {
+            Visitor visitor = visitorQueue.poll();  // Remove the visitor from the queue
+            visitorsRidden.add(visitor);  // Add the visitor to the ride history
+            System.out.println(visitor.getName() + " has taken the ride.");
+        }
+
+        numOfCycles++;  // Increment the number of cycles
+        System.out.println("Ride has been run for " + ridersThisCycle + " visitors. Total cycles: " + numOfCycles);
+    }
+
+    // part4a methods
+    /**
+     * Add a visitor to the history
+     * @param visitor
+     */
+    @Override
+    public void addVisitorToHistory(Visitor visitor) {
+        visitorsRidden.add(visitor);
+    }
+
+    /**
+     * Check if a visitor has ridden the ride before
+     * @param visitor
+     * @return true if the visitor has ridden the ride before, false otherwise
+     */
+    @Override
+    public boolean checkVisitorFromHistory(Visitor visitor) {
+        return visitorsRidden.contains(visitor);
+    }
+
+    /**
+     * Get the number of visitors
+     * @return the number of visitors
+     */
+    @Override
+    public int numberOfVisitors() {
+        return visitorsRidden.size();
+    }
+
+    /**
+     * Print the ride history
+     */
+    @Override
+    public void printRideHistory() {
+        if (visitorsRidden.isEmpty()) {
+            System.out.println("No visitors have taken the " + rideName + " yet.");
+        } else {
+            System.out.println("Ride history for " + rideName + ":");
+            Iterator<Visitor> iterator = visitorsRidden.iterator();
+            while (iterator.hasNext()) {
+                Visitor visitor = iterator.next();
+                System.out.println(visitor.getVisitorInfo());
+            }
+        }
+    }
+    // part4b methods
+    /**
+     * Sort the visitors who have ridden the ride by name and age
+     */
+    public void sortVisitors() {
+        Collections.sort(visitorsRidden, new VisitorComparator());
+        System.out.println("Visitors sorted by name and age.");
+    }
+
+    public void exportRideHistory(String fileName) {
+        BufferedWriter writer = null;
+
+        try {
+            // Create a new file object
+            File file = new File(fileName);
+
+            // Create a BufferedWriter to write data to the file
+            writer = new BufferedWriter(new FileWriter(file));
+
+            // Iterate through the ride history and write each visitor to the file
+            for (Visitor visitor : visitorsRidden){
+                writer.write(visitor.getVisitorInfo());
+                writer.newLine();  // Write each visitor on a new line
+            }
+
+            System.out.println("Ride history exported successfully to " + fileName);
+
+        } catch (IOException e) {
+            System.out.println("Error exporting ride history: " + e.getMessage());
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();  // Close the writer
+                }
+            } catch (IOException e) {
+                System.out.println("Error closing the file writer: " + e.getMessage());
+            }
+        }
+    }
+}
+
