f27eeb55f6a99ae6c5a23f60073fd636449bd9aa
2587132827 <2587132827@qq.com>
2024-12-16 05:14:10 +0800

    add: create new package for part 7, comments for method exportRideHistory

--- src/main/java/github/io/mli49/assignment/AssignmentTwo.java
+++ src/main/java/github/io/mli49/assignment/AssignmentTwo.java
@@ -185,5 +185,6 @@ public class AssignmentTwo {
     }
     public void partSeven(){
     }
+
 }
 
---
+++ src/main/java/github/io/mli49/assignment/part7/Visitor.java
@@ -0,0 +1,47 @@
+package github.io.mli49.assignment.part7;
+
+public class Visitor extends Person {
+    // 2 instance variable
+    /**
+     * The membership status of the visitor
+     */
+    private String membershipStatus;
+    /**
+     * The number of times the visitor has visited the park
+     */
+    private Integer visitCount;
+
+    // Default constructor
+    public Visitor() {}
+
+    // Constructor with parameters
+    public Visitor(String name, int age, String gender, String membershipStatus, Integer visitCount) {
+        super(name, age, gender); // Call the constructor of Person
+        this.membershipStatus = membershipStatus;
+        this.visitCount = visitCount;
+    }
+
+    // Getter and setter
+    public String getMembershipStatus() {
+        return membershipStatus;
+    }
+
+    public void setMembershipStatus(String membershipStatus) {
+        this.membershipStatus = membershipStatus;
+    }
+
+    public Integer getVisitCount() {
+        return visitCount;
+    }
+
+    public void setVisitCount(Integer visitCount) {
+        this.visitCount = visitCount;
+    }
+
+    // Additional method to display visitor info
+    public String getVisitorInfo() {
+        return "Visitor: " + getName() + ", Age: " + getAge() + ", Gender: " + getGender() +
+                ", Membership Status: " + membershipStatus+ ", Visit Count: " + visitCount;
+    }
+}
+
